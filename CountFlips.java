2f242735d839eac9b4182a833c3d345d40624a60
pkuselias <pkuselias@hartford.edu>
2025-02-27 16:56:20 -0500

    Form while loop for 100 flips

--- CountFlips.java
+++ CountFlips.java
@@ -1,6 +1,12 @@
 public class CountFlips {
   public static void main(String[] args) {
     Coin flippingCoin = new Coin();
+    int flipNumber = 1;
+    while (flipNumber < 101); {
+      flipNumber++;
+
+    }
+
 
   }
 }

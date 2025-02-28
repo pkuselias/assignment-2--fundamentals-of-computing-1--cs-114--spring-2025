102b0e359c0cc0c351f18f74f5faa2596e0d494d
pkuselias <pkuselias@hartford.edu>
2025-02-27 19:43:50 -0500

    Move code from incorrect file to correct file

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,17 +1,5 @@
-import java.util.Scanner;
-
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
 
-    System.out.println();
-    System.out.println("Please enter any string: ");
-
-    String userString;
-    userString = scan.next();
-
-    int stringLength = userString.length();
-
-    scan.close();
   }
 }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println();
+    System.out.println("Please enter any string: ");
+
+    String userString;
+    userString = scan.next();
+
+    int stringLength = userString.length();
+
+    scan.close();
   }
 }

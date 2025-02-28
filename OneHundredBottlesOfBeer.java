813fd7d2588192d89dbd159a4fed6bcfa778469f
pkuselias <pkuselias@hartford.edu>
2025-02-27 19:34:21 -0500

    Store user string

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
     System.out.println();
     System.out.println("Please enter any string: ");
+
+    String userString;
+    userString = scan.next();
+
+    scan.close();
   }
 }

aa124de34366186e9b3f5829772e27e315cb3253
pkuselias <pkuselias@hartford.edu>
2025-02-27 19:32:42 -0500

    Ask for user string

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -2,5 +2,7 @@ import java.util.Scanner;
 
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Please enter any string: ");
   }
 }

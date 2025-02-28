e076b4e269515f11d064a47cfa7086b6ea260b00
pkuselias <pkuselias@hartford.edu>
2025-02-27 19:16:51 -0500

    Add variables to tally results of flips

--- CountFlips.java
+++ CountFlips.java
@@ -2,8 +2,11 @@ public class CountFlips {
   public static void main(String[] args) {
     Coin flippingCoin = new Coin();
     int flipNumber = 1;
+    int totalHeads = 0;
+    int totalTails = 0;
     while (flipNumber < 101); {
       flippingCoin.flip();
+
       flipNumber++;
 
     }

e22a556702beb1f27ef688a1eb337b2c59905d32
pkuselias <pkuselias@hartford.edu>
2025-03-03 12:09:29 -0500

    Add invalid condition

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -5,11 +5,12 @@ public class OneHundredBottlesOfBeer {
     Scanner scan = new Scanner(System.in);
     System.out.println("Enter the number of verses to be printed: ");
     int versesNumber = scan.nextInt();
+    scan.close();
 
     if (versesNumber > 0 && versesNumber < 101) {
 
     } else {
-
+      System.out.println("The input for the number of verses is invalid.");
     }
   }
 }

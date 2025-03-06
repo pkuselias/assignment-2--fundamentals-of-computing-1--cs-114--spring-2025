3b656012002b9b19cabed8d7b3e3abd9a6553ffc
pkuselias <pkuselias@hartford.edu>
2025-03-05 19:32:23 -0500

    Rerun program upon invalid integer input

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -6,20 +6,22 @@ public class OneHundredBottlesOfBeer {
     System.out.println();
     System.out.println("Enter the number of verses to be printed: ");
     int versesNumber = scan.nextInt();
-    scan.close();
     System.out.println();
 
-    if (versesNumber > 0 && versesNumber < 101) {
-      for(int i = 0; i < versesNumber; i++) {
-        System.out.println((100-i) + " bottles of beer on the wall");
-        System.out.println((100-i) + " bottles of beer");
-        System.out.println("If one of those bottles should happen to fall");
-        System.out.println((100-i-1) + " bottles of beer on the wall");
-        System.out.println();
-      }
-    } else {
-      System.out.println("The input for the number of verses is invalid.");
+    while (versesNumber < 1 || versesNumber > 100) {
+      System.out.println("The input for the number of verses is invalid. Please re-enter a valid number of verses: ");
+      versesNumber = scan.nextInt();
       System.out.println();
     }
+
+    for(int i = 0; i < versesNumber; i++) {
+      System.out.println((100-i) + " bottles of beer on the wall");
+      System.out.println((100-i) + " bottles of beer");
+      System.out.println("If one of those bottles should happen to fall");
+      System.out.println((100-i-1) + " bottles of beer on the wall");
+      System.out.println();
+    }
+
+    scan.close();
   }
 }

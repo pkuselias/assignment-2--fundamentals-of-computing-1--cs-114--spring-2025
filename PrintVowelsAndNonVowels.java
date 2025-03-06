2c42860d2157dbfd0e986218d7983bcc13f1a6d8
pkuselias <pkuselias@hartford.edu>
2025-03-06 18:00:15 -0500

    Specify case of vowels counted

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -51,7 +51,7 @@ public class PrintVowelsAndNonVowels {
       }
     }
 
-    System.out.printf("a: %s%ne: %s%ni: %s%no: %s%nu: %s%n", smallA, smallE, smallI, smallO, smallU);
+    System.out.printf("Lowercase 'a': %s%nLowercase 'e': %s%nLowercase 'i': %s%nLowercase 'o': %s%nLowercase 'u': %s%n", smallA, smallE, smallI, smallO, smallU);
 
     System.out.println("Number of non-vowel characters: " + nonVowels);
 
